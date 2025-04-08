13a20e883dd8507bd87993280e6ba91a7af9746c
Nerish20021506 <20021506@students.koi.edu.au>
2025-04-08 14:17:46 +1000

    Average calculation
    This program calculates average of given grades.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0;
+            double gradecount = 3;
+            for (int gradecounter = 1; gradecounter <= 5; gradecounter++) 
+            {
+                Console.Write("Enter the grade: ");
+                double grade = Convert.ToDouble(Console.ReadLine());
+                total = total + grade;
+                gradecounter = gradecounter + 1;
+            }
+            Console.WriteLine("The average of the class is: " + total/gradecount);
+        }
+    }
+}
